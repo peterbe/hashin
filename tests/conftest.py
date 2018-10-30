4a659575b7fb973b1935ca049e191a79d89feb7b
Peter Bengtsson <mail@peterbe.com>
2018-10-29 21:20:34 -0400

    Refactor tests for pytest
    Fixes #82

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+from tempfile import mkdtemp
+from contextlib import contextmanager
+from shutil import rmtree
+
+
+import mock
+import pytest
+
+
+@pytest.fixture
+def murlopen():
+    with mock.patch("hashin.urlopen") as patch:
+        yield patch
+
+
+@pytest.fixture
+def mock_parser():
+    with mock.patch("hashin.parser") as patch:
+        yield patch
+
+
+@pytest.fixture
+def mock_sys():
+    with mock.patch("hashin.sys") as patch:
+        yield patch
+
+
+@pytest.fixture
+def mock_run():
+    with mock.patch("hashin.run") as patch:
+        yield patch
+
+
+@pytest.fixture
+def tmpfile():
+    @contextmanager
+    def inner(name="requirements.txt"):
+        dir_ = mkdtemp("hashintest")
+        try:
+            yield os.path.join(dir_, name)
+        finally:
+            rmtree(dir_)
+
+    return inner
