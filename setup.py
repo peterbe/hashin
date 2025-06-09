29eff8d47e213148402785393741a6ce8b8f35af
Sebastian Pipping <sebastian@pipping.org>
2025-06-09 23:50:52 +0200

    setup.py: Drop deprecated trove license classifier
    License classifier are deprecated since Setuptools >=77.0.0:
    - https://setuptools.pypa.io/en/stable/history.html#v77-0-0
    - https://github.com/pypa/setuptools/commit/0d8f1f2d12470efe3830e05f70996eb177287e26

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
